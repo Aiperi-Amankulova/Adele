3e86f05aa3c41d59c5f3e398041b0f64b997ffe0
Aiperi <aip.amankulova@gmail.com>
2021-02-28 19:30:39 +0600

    Main class for all fragments

---
+++ app/src/main/java/com/example/adele/data/common/BaseFragment.kt
@@ -0,0 +1,20 @@
+package com.example.adele.data.common
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+ abstract class BaseFragment : Fragment() {
+
+    protected abstract fun resID(): Int
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(resID(), container, false)
+    }
+}
