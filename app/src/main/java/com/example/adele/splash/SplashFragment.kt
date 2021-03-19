b84f13633dce973c18a4461cab013256ca9056c1
Aiperi <aip.amankulova@gmail.com>
2021-03-19 10:39:52 +0600

    Adding SplashFragment.kt

--- app/src/main/java/com/example/adele/splash/SplashFragment.kt
+++ app/src/main/java/com/example/adele/splash/SplashFragment.kt
@@ -5,27 +5,29 @@ import android.os.Bundle
 import android.os.Handler
 import android.view.View
 import androidx.navigation.fragment.findNavController
-import com.example.adele.MainActivity
+import com.example.adele.PreferenceHelper
 import com.example.adele.R
 import com.example.adele.data.common.BaseFragment
+import com.example.adele.ui.main.MainActivity
 
 class SplashFragment : BaseFragment() {
 
     override fun resID() = R.layout.activity_splash
-//
-//    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-//        super.onViewCreated(view, savedInstanceState)
-//        Handler().postDelayed({
-//            selectFragment()
-//        }, 3000)
-//    }
-//
-//    private fun selectFragment() {
-//        if (PreferenceHelper.getIsFirstLaunch()) {
-//            findNavController().navigate(R.id.action_splashFragment_to_onBoardMainFragment)
-//        } else {
-//            val intent = Intent(activity, MainActivity::class.java)
-//            startActivity(intent)
-//        }
-//    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        Handler().postDelayed({
+            selectFragment()
+        }, 3000)
+    }
+
+    private fun selectFragment() {
+        if (PreferenceHelper.getIsFirstLaunch()) {
+            findNavController().navigate(R.id.action_splashScreen2_to_onBoardMainFragment)
+        } else {
+            val intent = Intent(activity, MainActivity::class.java)
+            startActivity(intent)
+            activity?.onBackPressed()
+        }
+    }
 }
