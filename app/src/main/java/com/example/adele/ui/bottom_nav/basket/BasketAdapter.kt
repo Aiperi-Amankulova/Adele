6bc141543803091ce63203d0e073129b040e6cce
Aiperi <aip.amankulova@gmail.com>
2021-03-29 20:38:05 +0600

    Add Adapter for basket

---
+++ app/src/main/java/com/example/adele/ui/bottom_nav/basket/BasketAdapter.kt
@@ -0,0 +1,35 @@
+package com.example.adele.ui.bottom_nav.basket
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.example.adele.R
+import com.example.adele.data.model.HomeModel
+import com.example.adele.utils.diffUtils.DiffUtils
+import kotlinx.android.synthetic.main.item_basket.view.*
+
+class BasketAdapter(private val listener: () -> Unit) : ListAdapter<HomeModel, FavoriteViewHolder>(DiffUtils.diffUtilItems) {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FavoriteViewHolder {
+        val view = LayoutInflater.from(parent.context)
+                .inflate(R.layout.item_basket, parent, false)
+        return FavoriteViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: FavoriteViewHolder, position: Int) {
+        holder.bind(getItem(position), listener)
+    }
+}
+
+class FavoriteViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+    fun bind(item: HomeModel, listener: () -> Unit) {
+        itemView.textView9.text = item.title
+        itemView.imageView2.setImageResource(item.image)
+
+        itemView.item_basket.setOnClickListener {
+            listener.invoke()
+        }
+
+    }
+}
